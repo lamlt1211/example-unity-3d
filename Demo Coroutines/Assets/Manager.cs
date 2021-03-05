7f342c79740a418f95e5a91345e527c19ec6bdff
minhnq@monsterstudio.vn <minhnq@monsterstudio.vn>
2021-03-05 15:27:26 +0700

    Demo Coroutines/

---
+++ Demo Coroutines/Assets/Manager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+{
+    private float number;
+
+    public Text numberText;
+    public Button PlayButton;
+    public Button ResetButton;
+
+    public void StartNumber()
+    {
+        number = 10;
+        PlayButton.gameObject.SetActive(true);
+        StartCoroutine(RunNumber());
+    }
+
+    public void StopNumber()
+    {
+        PlayButton.gameObject.SetActive(true);
+        StopAllCoroutines();
+    }
+
+    private IEnumerator RunNumber()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            number--;
+            numberText.text = number.ToString();
+            if (number == 0)
+            {
+                StopAllCoroutines();
+            }
+        }
+    }
+}
