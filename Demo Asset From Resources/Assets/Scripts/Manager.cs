15ad221c95c4ccbb3e01dd6b341541ee2acde6ce
= <=>
2021-03-18 09:15:56 +0700

    demo

---
+++ Demo Asset From Resources/Assets/Scripts/Manager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Button PlayButton;
+    public SpriteRenderer spriteRenderer;
+
+   
+    public void Start()
+    {
+        PlayButton.gameObject.SetActive(true);
+    }
+
+    public void StartDino()
+    {
+        spriteRenderer.sprite = Resources.Load<Sprite>("test/Dino");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
